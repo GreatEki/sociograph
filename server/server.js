c9068c1df4f61443cdaefe3a300f2d9a633add14
Greateki <ekeneidiagbor@gmail.com>
2021-06-30 12:54:32 +0100

    chore: modularizing project. seperated resolver and typeDefs files

--- server/server.js
+++ server/server.js
@@ -1,42 +1,15 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 
 // The GraphQL schema
-const typeDefs = gql`
-    type User {
-        username: String!
-        email: String!
-    }
-
-    type Query {
-        getUsers: [User]
-    }
-`;
-
 // A map of functions which return data for the schema.
-const resolvers = {
-  Query: {
-    getUsers: () => {
-        const users = [
-            {
-                username: 'john',
-                email: 'john@email.com'
-            },
-            {
-                username: 'jane',
-                email: 'jane@email.com'
-            }
-        ]
-
-        return users;
-    }
-  },
-};
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+	console.log(`🚀 Server ready at ${url}`);
 });
