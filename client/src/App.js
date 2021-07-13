78be1d76494a05ac1e6eeab8047877efcf1df043
Greateki <ekeneidiagbor@gmail.com>
2021-07-13 15:05:41 +0100

    chore: decoupling the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,16 @@
-import { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Input from './component/Input';
-import Button from './component/Button';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import Register from './pages/Register';
 
 function App() {
-	const [user, setUser] = useState({
-		username: '',
-		email: '',
-		password: '',
-		confirmPassword: '',
-	});
-	function handleSubmit(e) {
-		e.preventDefault();
-		console.log(user);
-	}
-
-	const onChange = (e) => {
-		setUser({ ...user, [e.target.name]: e.target.value });
-	};
 	return (
-		<Container>
-			<Row className='container'>
-				<Col style={{ border: '2px solid orange' }}>
-					<div className='flex-center text-white'>
-						<h1> Register</h1>
-					</div>
-
-					<form onSubmit={handleSubmit}>
-						<Input
-							label={'Username'}
-							type={'text'}
-							name={'username'}
-							value={user.username}
-							onChange={onChange}
-						/>
-
-						<Input
-							label={'Email'}
-							type={'text'}
-							name={'email'}
-							value={user.email}
-							onChange={onChange}
-						/>
-						<Input
-							label={'Password'}
-							type={'password'}
-							name={'password'}
-							value={user.password}
-							onChange={onChange}
-						/>
-						<Input
-							label={'Confirm Password'}
-							type={'password'}
-							name={'confirmPassword'}
-							value={user.confirmPassword}
-							onChange={onChange}
-						/>
-
-						<div className='flex-center m5'>
-							<Button className={'btn-primary'}>Register</Button>
-						</div>
-					</form>
-				</Col>
-			</Row>
-		</Container>
+		<div>
+			<BrowserRouter>
+				<Switch>
+					<Route path={'/'} component={Register} />
+				</Switch>
+			</BrowserRouter>
+		</div>
 	);
 }
 
