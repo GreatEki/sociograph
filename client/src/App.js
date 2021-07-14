b01c4bd1748620e7eef69412a2fc1ca825bccf11
Greateki <ekeneidiagbor@gmail.com>
2021-07-14 12:52:45 +0100

    feat: added the login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
-import Register from './pages/Register';
 import ApolloProvider from './graphql/ApolloProvider';
+
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
 	return (
@@ -9,6 +11,8 @@ function App() {
 			<BrowserRouter>
 				<Switch>
 					<Route exact path={'/'} component={Register} />
+
+					<Route path={'/login'} component={Login} />
 				</Switch>
 			</BrowserRouter>
 		</ApolloProvider>
